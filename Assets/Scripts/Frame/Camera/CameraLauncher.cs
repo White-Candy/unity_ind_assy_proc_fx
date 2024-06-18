10c6fa24a91bfbd33501409505ffa6c9fd94339a
Sugar0612 <287276293@qq.com>
2024-06-18 23:32:31 +0800

    chore: 动画播放功能完成。

--- Assets/Scripts/Frame/Camera/CameraLauncher.cs
+++ Assets/Scripts/Frame/Camera/CameraLauncher.cs
@@ -18,10 +18,18 @@ public class CameraLauncher : MonoBehaviour
         CameraControl.CloseAll();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ModelAnimControl._Instance.Play(0, 100);
+        }
+    }
+
     void Start()
     {
         // 在重置玩家镜头
         CameraControl.SetNormal();
+        ModelAnimControl._Instance.Play(0, 0); // 这并不是为了 播放什么动画，只是为了重置场景模型
     }
-
 }
