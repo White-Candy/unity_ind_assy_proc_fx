0b652374de5a50988768d314a2c14caf0b2991ae
Sugar <287276293@qq.com>
2024-10-27 19:54:30 +0800

    chore:标题文本结构修改。

--- Assets/Scripts/UI/Panel/TitlePanel/TitlePanel.cs
+++ Assets/Scripts/UI/Panel/TitlePanel/TitlePanel.cs
@@ -55,6 +55,6 @@ public class TitlePanel : BasePanel
 
     public void SetTitle(string title)
     {
-        m_Title.text = $"{title}-{GlobalData.courseName}";
+        m_Title.text = $"{GlobalData.courseName}-{title}";
     }
 }
